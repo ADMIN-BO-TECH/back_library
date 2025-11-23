2ef15ffcf08f54e2cfcc8dd3020ae4b61526a494
Farouk212022 <faroukabdala21@gmail.com>
2025-11-23 18:03:12 -0500

    Implementation to return gradeLevel List in StudentRepository

--- src/main/java/co/com/botech/repository/StudentRepository.java
+++ src/main/java/co/com/botech/repository/StudentRepository.java
@@ -9,7 +9,16 @@ import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    /** Buscar estudiantes por school_id  */
+    /**
+     * Buscar estudiantes por school_id
+     */
     List<Student> findBySchool_Id(Long schoolId);
+
     Optional<Student> findById(Long id);
+
+    boolean existsByStudentIdAndSchool_Id(Long studentId, Long schoolId);
+
+    @Query("SELECT DISTINCT s.gradeLevel FROM Student s WHERE s.school.id = :schoolId")
+    List<String> findDistinctGradeLevelsBySchoolId(@Param("schoolId") Long schoolId);
+
 }
