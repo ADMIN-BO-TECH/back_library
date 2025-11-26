227c92631a7f2ca515758acbf4e81d20bfe9097f
Farouk212022 <faroukabdala21@gmail.com>
2025-11-25 22:09:22 -0500

    Add employee find by school id function

--- src/main/java/co/com/botech/repository/SchoolEmployeeRepository.java
+++ src/main/java/co/com/botech/repository/SchoolEmployeeRepository.java
@@ -1,8 +1,13 @@
 package co.com.botech.repository;
 
 import co.com.botech.entity.SchoolEmployee;
+import co.com.botech.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface SchoolEmployeeRepository extends JpaRepository<SchoolEmployee, Long> {
     boolean existsByDocumentNumberAndSchool_Id(String documentNumber, Long schoolId);
+
+    List<SchoolEmployee> findBySchool_Id(Long schoolId);
 }
