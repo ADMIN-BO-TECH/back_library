76f929d72b59ed17887764230ee5bfe6366f16ed
Farouk212022 <faroukabdala21@gmail.com>
2025-11-25 21:37:18 -0500

    Add function to Employees to find by document number and schoolId

--- src/main/java/co/com/botech/repository/SchoolEmployeeRepository.java
+++ src/main/java/co/com/botech/repository/SchoolEmployeeRepository.java
@@ -3,4 +3,6 @@ package co.com.botech.repository;
 import co.com.botech.entity.SchoolEmployee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SchoolEmployeeRepository extends JpaRepository<SchoolEmployee, Long> {}
+public interface SchoolEmployeeRepository extends JpaRepository<SchoolEmployee, Long> {
+    boolean existsByDocumentNumberAndSchool_Id(String documentNumber, Long schoolId);
+}
