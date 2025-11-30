216de3b43c0d8155c3c020f95a7413d6d4eeaf26
Farouk212022 <faroukabdala21@gmail.com>
2025-11-30 08:22:09 -0500

    Update Authorization Repository

--- src/main/java/co/com/botech/repository/AuthorizationRepository.java
+++ src/main/java/co/com/botech/repository/AuthorizationRepository.java
@@ -1,6 +1,17 @@
 package co.com.botech.repository;
 
 import co.com.botech.entity.Authorization;
+import co.com.botech.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface AuthorizationRepository extends JpaRepository<Authorization, Long> {}
+import java.util.List;
+
+public interface AuthorizationRepository extends JpaRepository<Authorization, Long> {
+    @Query("SELECT a FROM Authorization a WHERE a.id = :authorizedPersonId AND (NOW() BETWEEN a.authorizationStartDate AND a.authorizationEndDate)")
+    List<Authorization> findDateActiveAuthorizationsByAuthorizedPersonId(@Param("authorizedPersonId") Long authorizedPersonId);
+
+    @Query("SELECT a FROM Authorization a WHERE a.authorizedPerson.school.id = :schoolId")
+    List<Authorization> findBySchool_Id(Long schoolId);
+}
