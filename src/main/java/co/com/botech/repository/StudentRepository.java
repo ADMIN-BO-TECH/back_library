81b10b7db21718db10e5e86f6a31df1acffdb4dc
Farouk212022 <faroukabdala21@gmail.com>
2025-12-06 11:15:56 -0500

    Notification Queries

--- src/main/java/co/com/botech/repository/StudentRepository.java
+++ src/main/java/co/com/botech/repository/StudentRepository.java
@@ -20,9 +20,11 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     boolean existsByStudentIdAndSchool_Id(Long studentId, Long schoolId);
     boolean existsByIdAndStudentId(Long studentRecordId, Long studentId);
 
+    @Query("SELECT DISTINCT s.familyCode.id FROM Student s WHERE s.gradeLevel IN :gradeList")
+    List<Long> findFamilyCodesByGrades(@Param("gradeList") List<String> gradeList);
+
     @Query("SELECT DISTINCT s.gradeLevel FROM Student s WHERE s.school.id = :schoolId")
     List<String> findDistinctGradeLevelsBySchoolId(@Param("schoolId") Long schoolId);
 
     List<Student> findByFamilyCode_Id (Long familyCodeId);
-
 }
--- src/main/java/co/com/botech/repository/UserRepository.java
+++ src/main/java/co/com/botech/repository/UserRepository.java
@@ -2,5 +2,27 @@ package co.com.botech.repository;
 
 import co.com.botech.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT DISTINCT u FROM User u " +
+            "JOIN UserNotificationPreferences up ON  up.user.id = u.id WHERE " +
+            "(u.school.id = :schoolId) AND " +
+            "(up.enabled = true) AND " +
+            "(up.category.id = :notificationCategoryId)")
+    List<User> findUserBySchool_IdAndActivePreference(@Param("schoolId") Long schoolId,
+                                                      @Param("notificationCategoryId") Long notificationCategoryId);
+
+    @Query("SELECT DISTINCT u FROM User u " +
+            "JOIN UserNotificationPreferences up ON  up.user.id = u.id WHERE " +
+            "(u.school.id = :schoolId) AND " +
+            "(up.enabled = true) AND " +
+            "(up.category.id = :notificationCategoryId) AND " +
+            "(u.familyCode.id IN :familyCodeList)")
+    List<User> findUserBySchool_IdAndActivePreferenceAndFamilyCode(@Param("schoolId") Long schoolId,
+                                                                   @Param("notificationCategoryId") Long notificationCategoryId,
+                                                                   @Param("familyCodeList") List<Long> familyCodeList);
+}
