835ad39c8152a53676ea8b1e300b18d4f73f4672
SebassPP <juanpapa@unisabana.edu.co>
2026-01-30 10:53:31 -0500

    Agrega relaciones faltantes en Route version=0.1.15

--- src/main/java/co/com/botech/entity/Route.java
+++ src/main/java/co/com/botech/entity/Route.java
@@ -3,27 +3,56 @@ package co.com.botech.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
-@Entity @Table(name = "route")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "route")
 public class Route {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "route_id")
     private Long id;
 
-    @Column(name="route_name") private String routeName;
-    @Column(name="route_days") private String routeDays;
+    @Column(name = "route_name")
+    private String routeName;
 
-    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "vehicle_id")
+    @Column(name = "route_days")
+    private String routeDays;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehicle_id")
     private Vehicle vehicle;
 
-    @Column(name="status")     private Boolean status;
-    @Column(name="assistant_id") private Integer assistantId;
-    @Column(name="operator_id")  private Integer operatorId;
+    @Column(name = "status")
+    private Boolean status;
 
-    @Column(name="start_time") private String startTime;
-    @Column(name="end_time")   private String endTime;
-    @Column(name="route_type") private String routeType;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "assistant_id",
+            nullable = true
+    )
+    private Employee assistant;
 
-    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "school_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "operator_id",
+            nullable = true
+    )
+    private Employee operator;
+
+    @Column(name = "start_time")
+    private String startTime;
+
+    @Column(name = "end_time")
+    private String endTime;
+
+    @Column(name = "route_type")
+    private String routeType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "school_id")
     private School school;
 }
