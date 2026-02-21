c06f7d567def8d5e5668518e0239a22fd706f6a8
Farouk212022 <faroukabdala21@gmail.com>
2026-02-21 18:02:35 -0500

    Update Library version=0.1.72

---
+++ src/main/java/co/com/botech/dto/attendance/AttendanceExcelObject.java
@@ -0,0 +1,35 @@
+package co.com.botech.dto.attendance;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AttendanceExcelObject {
+
+    @JsonProperty("Id de Registro")
+    private Long attendanceRegisterId;
+    @JsonProperty("Fecha y Hora")
+    private LocalDateTime attendanceDateTime;
+    @JsonProperty("Código de Colegio/Documento")
+    private String schoolIdentifier;
+    @JsonProperty("Tipo de Usuario")
+    private String typeUser;
+    @JsonProperty("Nombre")
+    private String userName;
+    @JsonProperty("Curso/Categoría")
+    private String category;
+    @JsonProperty("Nombre de Sistema")
+    private String systemName;
+    @JsonProperty("Tipo de Registro")
+    private String registerType;
+    @JsonProperty("Tag de Lector")
+    private String rfidTag;
+    @JsonProperty("Descripción de Lector")
+    private String rfidDescription;
+}
