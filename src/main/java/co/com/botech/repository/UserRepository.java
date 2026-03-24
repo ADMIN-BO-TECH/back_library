00f3876ff359b36f1b2b1853ee8382b1cf6ffc9b
SebassPP <juanpapa@unisabana.edu.co>
2026-03-24 09:59:46 -0500

    Change some querys in UserRepository

--- src/main/java/co/com/botech/repository/UserRepository.java
+++ src/main/java/co/com/botech/repository/UserRepository.java
@@ -9,37 +9,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("""
-        SELECT DISTINCT u
-        FROM User u
-        JOIN UserNotificationPreferences up ON up.user = u
-        JOIN UserFamily uf ON uf.user = u
-        JOIN uf.family f
-        WHERE f.school.id = :schoolId
-          AND up.enabled = true
-          AND up.category.id = :notificationCategoryId
-    """)
-    List<User> findUsersWithActivePreferenceBySchool(
-            @Param("schoolId") Long schoolId,
-            @Param("notificationCategoryId") Long notificationCategoryId
-    );
-
-    @Query("""
-        SELECT DISTINCT u
-        FROM User u
-        JOIN UserNotificationPreferences up ON up.user = u
-        JOIN UserFamily uf ON uf.user = u
-        JOIN uf.family f
-        WHERE f.school.id = :schoolId
-          AND up.enabled = true
-          AND up.category.id = :notificationCategoryId
-          AND f.id IN :familyIdList
-    """)
-    List<User> findUsersWithActivePreferenceBySchoolAndFamilies(
-            @Param("schoolId") Long schoolId,
-            @Param("notificationCategoryId") Long notificationCategoryId,
-            @Param("familyIdList") List<Long> familyIdList
-    );
 
     Optional<User> findByFirebaseUid(String firebaseUid);
 
@@ -48,5 +17,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT DISTINCT uf.user FROM UserFamily uf WHERE uf.family.school.id = :schoolId AND uf.family.id IN :familyIds")
     List<User> findUsersBySchoolAndFamilies(@Param("schoolId") Long schoolId, @Param("familyIds") List<Long> familyIds);
-
 }
