2a69bfb2aa84cc5c103378975bf6360815d489fd
Farouk212022 <faroukabdala21@gmail.com>
2026-04-11 12:48:55 -0500

    Update Waypoints Request

--- src/main/java/co/com/botech/dto/routeWaypoint/UpdateRouteWaypointRequest.java
+++ src/main/java/co/com/botech/dto/routeWaypoint/UpdateRouteWaypointRequest.java
@@ -1,18 +1,32 @@
 package co.com.botech.dto.routeWaypoint;
 
-import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.util.List;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class UpdateRouteWaypointRequest {
-    @Valid
-    List<UpdateRouteWaypointDTO> routeWaypoints;
+    @NonNull
+    @Min(value = 1, message = "Los ID's deben ser positivos y enteros")
+    private Long id;
+    @Pattern(regexp = "^[A-Za-z0-9 áéíóúÁÉÍÓÚñÑ_().#,/*\\r\\n-:]+$", message = "Los nombres de paradas no puede contener caracteres especiales")
+    private String name;
+    @Min(value = 1, message = "El id del recorrido debe ser un número positivo")
+    private Long routeId;
+
+    @Min(value = 1, message = "El orden de parada debe ser un número positivo")
+    private int stopOrder;
+
+    @Min(value = 1, message = "El orden de parada especifico debe ser un número positivo")
+    private int specificOrder;
+
+    @Pattern(regexp = "^[A-Za-z0-9 áéíóúÁÉÍÓÚñÑ_().#,/*\\r\\n-:]+$", message = "La dirección no puede contener caracteres especiales")
+    private String address;
+
+    private double latitude;
+
+    private double longitude;
 }
