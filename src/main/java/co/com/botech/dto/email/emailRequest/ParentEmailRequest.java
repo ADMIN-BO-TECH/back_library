a2b8ed77650ea380368e0cf10165660896cba424
Farouk212022 <faroukabdala21@gmail.com>
2026-04-19 11:40:04 -0500

    Update Requests

---
+++ src/main/java/co/com/botech/dto/email/emailRequest/ParentEmailRequest.java
@@ -0,0 +1,19 @@
+package co.com.botech.dto.email.emailRequest;
+
+import co.com.botech.dto.email.EmailVariables;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ParentEmailRequest{
+    @Pattern(regexp = "^[A-Za-z0-9 áéíóúÁÉÍÓÚñÑ_().#,/*\\r\\n-:]+$", message = "El codigo de familia contiene caracteres inválidos")
+    private String familyCode;
+    @NotEmpty
+    private List<@Email(message = "El formato del correo no es válido") String> emails;
+}
