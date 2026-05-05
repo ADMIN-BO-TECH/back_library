1ad8c2d7451f77c732e40ab823d8a8d9ff7f235d
SebassPP <juanpapa@unisabana.edu.co>
2026-05-05 09:31:36 -0500

    Add Lombok annotations to QRVehicleRequest for boilerplate reduction

--- src/main/java/co/com/botech/dto/QRManagement/QRVehicleRequest.java
+++ src/main/java/co/com/botech/dto/QRManagement/QRVehicleRequest.java
@@ -1,5 +1,12 @@
 package co.com.botech.dto.QRManagement;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class QRVehicleRequest {
     private String document;
     private Long idVehicle;
