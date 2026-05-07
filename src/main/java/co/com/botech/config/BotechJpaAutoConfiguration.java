64bcc34287d0800148bd6a6ac8adbb7877c03224
SebassPP <juanpapa@unisabana.edu.co>
2026-05-07 17:13:02 -0500

    Update BotechJpaAutoConfiguration to use @EntityScan annotation and remove deprecated methods

--- src/main/java/co/com/botech/config/BotechJpaAutoConfiguration.java
+++ src/main/java/co/com/botech/config/BotechJpaAutoConfiguration.java
@@ -7,27 +7,20 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProce
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.domain.EntityScanPackages;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @AutoConfiguration
 @ConditionalOnClass({Entity.class, JpaRepository.class})
+@EntityScan(basePackages = "co.com.botech.entity")
 public class BotechJpaAutoConfiguration {
 
-    /**
-     * Registra los paquetes de la librería en:
-     * - EntityScanPackages → para que Hibernate encuentre las entidades
-     * - AutoConfigurationPackages → para que JpaRepositoriesAutoConfiguration encuentre los repos
-     *
-     * Declarado static para que Spring lo procese antes de instanciar cualquier otro bean.
-     */
     @Bean
     public static BeanDefinitionRegistryPostProcessor botechJpaPackagesRegistrar() {
         return new BeanDefinitionRegistryPostProcessor() {
             @Override
             public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) {
-                EntityScanPackages.register(registry, "co.com.botech.entity");
                 AutoConfigurationPackages.register(registry, "co.com.botech.repository");
             }
 
