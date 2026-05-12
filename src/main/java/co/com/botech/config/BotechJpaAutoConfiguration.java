815ae5252944a84c3b6d43c529e295a218e4ca35
SebassPP <juanpapa@unisabana.edu.co>
2026-05-12 11:50:29 -0500

    Update BotechJpaAutoConfiguration to enable JPA repositories and simplify configuration

--- src/main/java/co/com/botech/config/BotechJpaAutoConfiguration.java
+++ src/main/java/co/com/botech/config/BotechJpaAutoConfiguration.java
@@ -1,34 +1,17 @@
 package co.com.botech.config;
 
 import jakarta.persistence.Entity;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @AutoConfiguration
 @ConditionalOnClass({Entity.class, JpaRepository.class})
 @EntityScan(basePackages = "co.com.botech.entity")
+@EnableJpaRepositories(basePackages = "co.com.botech.repository")
 @ComponentScan(basePackages = "co.com.botech.util")
 public class BotechJpaAutoConfiguration {
-
-    @Bean
-    public static BeanDefinitionRegistryPostProcessor botechJpaPackagesRegistrar() {
-        return new BeanDefinitionRegistryPostProcessor() {
-            @Override
-            public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) {
-                AutoConfigurationPackages.register(registry, "co.com.botech.repository");
-            }
-
-            @Override
-            public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) {
-            }
-        };
-    }
 }
