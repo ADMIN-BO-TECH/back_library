6e45d613a89adb777a76519b1f3d3a2fef7feb65
SebassPP <juanpapa@unisabana.edu.co>
2026-05-12 11:39:01 -0500

    Add @ComponentScan annotation to BotechJpaAutoConfiguration for utility package scanning

--- src/main/java/co/com/botech/config/BotechJpaAutoConfiguration.java
+++ src/main/java/co/com/botech/config/BotechJpaAutoConfiguration.java
@@ -9,11 +9,13 @@ import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @AutoConfiguration
 @ConditionalOnClass({Entity.class, JpaRepository.class})
 @EntityScan(basePackages = "co.com.botech.entity")
+@ComponentScan(basePackages = "co.com.botech.util")
 public class BotechJpaAutoConfiguration {
 
     @Bean
