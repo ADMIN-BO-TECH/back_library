e8dbec34cfc6e47c7fef2c1cc07ec9f2136e1592
SebassPP <juanpapa@unisabana.edu.co>
2026-05-12 14:28:57 -0500

    Refactor BotechJpaAutoConfiguration to simplify configuration and remove unused bean registration

--- src/main/java/co/com/botech/config/BotechJpaAutoConfiguration.java
+++ src/main/java/co/com/botech/config/BotechJpaAutoConfiguration.java
@@ -1,37 +1,13 @@
 package co.com.botech.config;
 
 import jakarta.persistence.Entity;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@AutoConfiguration(beforeName = {
-        "org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration",
-        "org.springframework.boot.data.jpa.autoconfigure.DataJpaRepositoriesAutoConfiguration"
-})
+@AutoConfiguration
 @ConditionalOnClass({Entity.class, JpaRepository.class})
 @ComponentScan(basePackages = "co.com.botech.util")
 public class BotechJpaAutoConfiguration {
-
-    @Bean
-    public static BeanDefinitionRegistryPostProcessor botechJpaPackagesRegistrar() {
-        return new BeanDefinitionRegistryPostProcessor() {
-            @Override
-            public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) {
-                AutoConfigurationPackages.register(registry,
-                        "co.com.botech.entity",
-                        "co.com.botech.repository"
-                );
-            }
-
-            @Override
-            public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) {}
-        };
-    }
 }
