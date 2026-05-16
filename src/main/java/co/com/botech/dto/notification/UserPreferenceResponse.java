7eb7c0f2cdd6421b9b08b65d3dd7992d4e59b331
Farouk212022 <faroukabdala21@gmail.com>
2026-05-16 12:35:16 -0500

    Update preferences

--- src/main/java/co/com/botech/dto/notification/UserPreferenceResponse.java
+++ src/main/java/co/com/botech/dto/notification/UserPreferenceResponse.java
@@ -7,7 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class UpdateCategoryPreferenceDTO {
+public class UserPreferenceResponse {
     private String categoryName;
     private Boolean enabled;
 }
