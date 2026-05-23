793611efa37cc77b796ff06a650d513588084c5c
Farouk212022 <faroukabdala21@gmail.com>
2026-05-23 11:29:16 -0500

    Update fix for schedule testing

--- src/main/java/co/com/botech/security/SpringSecurityConfig.java
+++ src/main/java/co/com/botech/security/SpringSecurityConfig.java
@@ -25,6 +25,7 @@ public class SpringSecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/schedule/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}))
