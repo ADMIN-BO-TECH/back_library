02e2ce1230bbe8e90fd17bd3046390e88a61decb
SebassPP <juanpapa@unisabana.edu.co>
2026-05-25 12:46:12 -0400

    Add CreatePermitResponse DTO and update PermitTypeConstants with new permit types

---
+++ src/main/java/co/com/botech/dto/permit/CreatePermitResponse.java
@@ -0,0 +1,13 @@
+package co.com.botech.dto.permit;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreatePermitResponse {
+    private String message;
+    private String email;
+}
