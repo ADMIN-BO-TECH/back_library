efbb80bb92ff6c3c0dfb59bd219cfca0eeda24d9
SebassPP <juanpapa@unisabana.edu.co>
2026-06-22 16:12:07 -0400

    Add Otp path

--- src/main/java/co/com/botech/security/SpringSecurityConfig.java
+++ src/main/java/co/com/botech/security/SpringSecurityConfig.java
@@ -27,6 +27,7 @@ public class SpringSecurityConfig {
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/schedule/**","/family/**").permitAll()
                         .requestMatchers("/api/passes/**").permitAll()
+                        .requestMatchers("/api/otp/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}))
