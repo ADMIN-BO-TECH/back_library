196d14a13f41d1adee14aba35e665d46f3d25db5
SebassPP <juanpapa@unisabana.edu.co>
2026-08-24 13:32:19 -0500

    Add conditional annotations to BotechGeocodingAutoConfiguration for GeoApiContext and GeocodingService

--- src/main/java/co/com/botech/config/BotechGeocodingAutoConfiguration.java
+++ src/main/java/co/com/botech/config/BotechGeocodingAutoConfiguration.java
@@ -3,16 +3,18 @@ package co.com.botech.config;
 import com.google.maps.GeoApiContext;
 import co.com.botech.util.geocoding.GeocodingService;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
+@ConditionalOnClass(GeoApiContext.class)                                    // ← NUEVO
 @ConditionalOnExpression("#{environment.containsProperty('GEOCODING_API_KEY')}")
 public class BotechGeocodingAutoConfiguration {
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(GeoApiContext.class)
     public GeoApiContext geoApiContext() {
         return new GeoApiContext.Builder()
                 .apiKey(System.getenv("GEOCODING_API_KEY"))
@@ -20,7 +22,7 @@ public class BotechGeocodingAutoConfiguration {
     }
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(GeocodingService.class)
     public GeocodingService geocodingService(GeoApiContext geoApiContext) {
         return new GeocodingService(geoApiContext);
     }
