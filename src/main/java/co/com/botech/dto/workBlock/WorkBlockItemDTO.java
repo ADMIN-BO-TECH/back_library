01895280791a9e954a61522292fa34603464a37d
SebassPP <juanpapa@unisabana.edu.co>
2026-09-01 10:01:32 -0500

    Add blockDate field to WorkBlockItemDTO for date handling

--- src/main/java/co/com/botech/dto/workBlock/WorkBlockItemDTO.java
+++ src/main/java/co/com/botech/dto/workBlock/WorkBlockItemDTO.java
@@ -3,6 +3,7 @@ package co.com.botech.dto.workBlock;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
@@ -13,6 +14,7 @@ public class WorkBlockItemDTO {
     private Integer minutesExtra;
     private Boolean nigthHours;
     private Integer minutesNigthHours;
+    @JsonFormat(pattern = "yyyy-MM-dd") private LocalDate blockDate;
     @JsonFormat(pattern = "HH:mm:ss") private LocalTime startTime;
     @JsonFormat(pattern = "HH:mm:ss") private LocalTime endTime;
     private Integer     durationMinutes;
